b685adbc2e04baa3c88ee09145ea0edb28a39a5e
Slav Lalev <slalev57@me.com>
2019-11-16 19:07:23 +0000

    Added customers to our customers table

---
+++ db/seeds/01_customers.sql
@@ -0,0 +1,6 @@
+-- Adding customers to our customers table
+
+INSERT INTO customers (id, name, email, phone_number, credit_card)
+VALUES ('1', 'Serge', 'serge95@gmail.com', '514-874-158', 5687459874325486);
+
+
