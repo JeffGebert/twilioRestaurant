bafd5321235c602752a7c6fe0640ed9e7aa14e36
Slav Lalev <slalev57@me.com>
2019-11-16 19:43:24 +0000

    Added all our items to items tables, without the pictures

---
+++ db/seeds/02_items.sql
@@ -0,0 +1,40 @@
+-- Widgets table seeds here (Example)
+INSERT INTO widgets (name, user_id) VALUES ('Sprockets', 1);
+INSERT INTO widgets (name, user_id) VALUES ('Chains', 2);
+INSERT INTO widgets (name, user_id) VALUES ('Bearings', 2);
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('1', 'Crunchy Burger', 8, '', 'A delicious burger with a crunch made from our finest beef');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('2', 'All Dressed Omelette', 12, '', 'Anything you can imagine in an omelette');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('3', 'Fish n Chips', 12, '', 'Cant go wrong with this classic !');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('4', 'Chicken Shawarma', 11, '', 'Healthy and tasty');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('5', 'Chicken and Noodles soup', 6, '', 'Made from the original recipie');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('6', 'Lasagna Bologna', 9, '', 'Homemade lasagna, freshly made every day');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('7', 'Classic Poutine', 7, '', 'Classic style french fries with lots of cheese and gravy !');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('8', 'Beef Tacos', 10, '', 'Beef Tacos made homemade style');
+
+
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('9', 'Exotic Venezuelan Sausage', 5, '', 'Imported from Venezuela the best thing you will ever taste');
