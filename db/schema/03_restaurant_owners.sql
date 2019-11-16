a7cb10462e28937a0cc79c65de76b8b2f1faa130
Slav Lalev <slalev57@me.com>
2019-11-16 16:44:39 +0000

    Created restaurant owners table inside the database

---
+++ db/schema/03_restaurant_owners.sql
@@ -0,0 +1,7 @@
+-- Creating the restaurant owners table
+
+DROP TABLE IF EXISTS restaurant_owners CASCADE;
+CREATE TABLE restaurant_owners (
+id SERIAL PRIMARY KEY NOT NULL,
+restaurant_name VARCHAR(100),
+);
