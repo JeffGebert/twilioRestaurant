ae5faff8fe7cf769e57d35754b10d81e8587aa93
Slav Lalev <slalev57@me.com>
2019-11-16 18:47:28 +0000

    Created orders items table inside the database

---
+++ db/schema/04_orders_items.sql
@@ -0,0 +1,7 @@
+-- Creating a orders items table
+
+DROP TABLE IF EXISTS orders_items CASCADE;
+CREATE TABLE orders_items (
+id SERIAL PRIMARY KEY NOT NULL,
+item_id INTEGER REFERENCES items (id) ON DELETE CASCADE
+);
