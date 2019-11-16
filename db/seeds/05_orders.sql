c898f8d127c52bc43504084d4366c4f492477a1a
Slav Lalev <slalev57@me.com>
2019-11-16 20:20:51 +0000

    changed the ordered things id to orders_items_id

--- db/seeds/05_orders.sql
+++ db/seeds/05_orders.sql
@@ -1,2 +1,2 @@
-INSERT INTO orders (id, customer_id, restaurant_owner_id, ordered_things_id, time_to_make)
+INSERT INTO orders (id, customer_id, restaurant_owner_id, orders_items_id, time_to_make)
 VALUES ('1', '1', '1', '1', 25 );
