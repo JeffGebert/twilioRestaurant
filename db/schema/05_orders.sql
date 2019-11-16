512a3df93e2d5e8a03ed0a5d284cbba927f80d9c
Slav Lalev <slalev57@me.com>
2019-11-16 18:56:11 +0000

    Created orders table inside the database

---
+++ db/schema/05_orders.sql
@@ -0,0 +1,9 @@
+-- Creating orders table
+
+DROP TABLE IF EXISTS orders CASCADE;
+CREATE TABLE orders (
+  id SERIAL PRIMARY KEY,
+  customer_id INTEGER REFERENCES customers (id) ON DELETE CASCADE,
+  restaurant_owner_id INTEGER REFERENCES restaurant_owners (id) ON DELETE CASCADE,
+  time_to_make SMALLINT DEFAULT NULL,
+);
