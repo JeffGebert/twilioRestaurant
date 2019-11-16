9bcefc4ecbca8148cb9631575b3767ebaf4d4c92
Jeff Gebert <jeffgebert@hotmail.com>
2019-11-16 19:38:08 +0000

    created checkout, menu, owner routes

--- routes/checkout.js
+++ routes/checkout.js
@@ -10,18 +10,9 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    let query = `SELECT * FROM widgets`;
-    console.log(query);
-    db.query(query)
-      .then(data => {
-        const widgets = data.rows;
-        res.json({ widgets });
-      })
-      .catch(err => {
-        res
-          .status(500)
-          .json({ error: err.message });
-      });
+    res.render("checkout");
   });
   return router;
 };
+
+
