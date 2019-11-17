f9919397c9eb82d4d76d141a19a27f1fdd757f2d
Slav Lalev <slalev57@me.com>
2019-11-17 16:53:50 +0000

    Added not null after restaurant_name

--- db/schema/03_restaurant_owners.sql
+++ db/schema/03_restaurant_owners.sql
@@ -3,5 +3,5 @@
 DROP TABLE IF EXISTS restaurant_owners CASCADE;
 CREATE TABLE restaurant_owners (
 id SERIAL PRIMARY KEY NOT NULL,
-restaurant_name VARCHAR(100)
+restaurant_name VARCHAR(100) NOT NULL
 );
