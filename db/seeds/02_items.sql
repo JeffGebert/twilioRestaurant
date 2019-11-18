7a36f9f9995756e6ad12f108ba06de042ba04646
Slav Lalev <slalev57@me.com>
2019-11-18 16:00:43 +0000

    added pictures to our seends from unsplash.com

--- db/seeds/02_items.sql
+++ db/seeds/02_items.sql
@@ -5,33 +5,35 @@ VALUES ('1', 'Crunchy Burger', 8, 'https://images.unsplash.com/photo-15689013463
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('2', 'All Dressed Omelette', 12, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Anything you can imagine in an omelette');
+VALUES ('2', 'All Dressed Omelette', 12, 'https://images.unsplash.com/photo-1510693206972-df098062cb71?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1201&q=80', 'Anything you can imagine in an omelette');
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('3', 'Fish n Chips', 12, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Cant go wrong with this classic !');
+VALUES ('3', 'Fish n Chips', 12, 'https://images.unsplash.com/photo-1553557202-e8e60357f061?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1267&q=80', 'Cant go wrong with this classic !');
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('4', 'Chicken Shawarma', 11, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Healthy and tasty');
+VALUES ('4', 'Chicken Shawarma', 11, 'https://images.unsplash.com/photo-1561651823-34feb02250e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=778&q=80', 'Healthy and tasty');
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('5', 'Chicken and Noodles soup', 6, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Made from the original recipie');
+VALUES ('5', 'Chicken and Noodles soup', 6, 'https://images.unsplash.com/photo-1569058242567-93de6f36f8e6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 'Made from the original recipie');
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('6', 'Lasagna Bologna', 9, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Homemade lasagna, freshly made every day');
+VALUES ('6', 'Lasagna Bologna', 9, 'https://images.unsplash.com/photo-1560750133-c5d4ef4de911?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80', 'Homemade lasagna, freshly made every day');
+
+
+--no picture
+INSERT INTO items (id, name, price, image_url, description)
+VALUES ('7', 'Classic Poutine', 7, 'https://cottagelife.com/wp-content/uploads/2014/08/Foodio.jpg', 'Classic style french fries with lots of cheese and gravy !');
+
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('7', 'Classic Poutine', 7, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Classic style french fries with lots of cheese and gravy !');
+VALUES ('8', 'Beef Tacos', 10, 'https://images.unsplash.com/photo-1552332386-f8dd00dc2f85?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80', 'Beef Tacos made homemade style');
 
 
 INSERT INTO items (id, name, price, image_url, description)
-VALUES ('8', 'Beef Tacos', 10, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Beef Tacos made homemade style');
+VALUES ('9', 'Exotic Venezuelan Sausage', 5, 'https://images.unsplash.com/photo-1552913903-2cffa1962dc7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80', 'Imported from Venezuela');
 
-
-INSERT INTO items (id, name, price, image_url, description)
-VALUES ('9', 'Exotic Venezuelan Sausage', 5, 'https://images.unsplash.com/photo-1568901346375-23c9450c58cd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1502&q=80', 'Imported from Venezuela');
---adedd comment
