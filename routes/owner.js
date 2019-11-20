db9e95433fb703904812c14bfd18c9327f29c897
Slav Lalev <slalev57@me.com>
2019-11-20 17:58:18 +0000

    added some scc to the owner's page and some code on it's route

--- routes/owner.js
+++ routes/owner.js
@@ -10,7 +10,34 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
+
     res.render("owners");
   });
+
+  router.get('/orders.id', (re, res) => {
+
+    //To display the person's order onto the page
+    db.query('SELECT * FROM orders RETURNING id', values)
+      .then(res => {
+        let order_id = res.rows[0].id;
+
+      })
+//to get the time to make from the text area
+let values = [`${req.body.waitingTime}`]
+db.query(`INSERT INTO orders (time_to_make) VALUES ($1)`,values)
+
+  })
   return router;
 };
+
+
+// generates a random alphanumerical string of 6 characters
+function generateRandomString() {
+  var text = "";
+  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+  for (var i = 0; i < 6 ; i++)
+  text += charset.charAt(Math.floor(Math.random() * charset.length));
+
+  return text;
+}
+
