8a79190500d4a6b31db872c8c94ae13125d52a1c
Slav Lalev <slalev57@me.com>
2019-11-20 22:41:20 +0000

    fetching the customer name to the orders page and displaying it

--- routes/owner.js
+++ routes/owner.js
@@ -18,7 +18,7 @@ module.exports = (db) => {
 
     //To display the person's order onto the page
     let values = [req.params.id]
-    console.log(req.params.id)
+
     db.query('SELECT * FROM orders WHERE id = $1', values)
       .then(data => {
         const order = data.rows[0]
@@ -27,15 +27,22 @@ module.exports = (db) => {
        db.query(`SELECT * FROM orders_items INNER JOIN items ON items.id = item_id WHERE order_id = $1`, values)
        .then(data => {
          order.items = data.rows
-         console.log("order", order);
-         res.render('owners', {order})
+           let values2 = [order.customer_id]
+
+           db.query(`SELECT orders.id, orders.customer_id, customers.name FROM orders JOIN customers ON orders.customer_id = customers.id WHERE customer_id = $1`,values2)
+           .then(data => {
+             order["customer_name"] = data.rows[0].name
+             console.log("order", order.customer_name);
+             res.render('owners', {order})
+            })
+           })
        })
       })
 // //to get the time to make from the text area
 // let values1 = [`${req.body.waitingTime}`]
 // db.query(`INSERT INTO orders (time_to_make) VALUES ($1)`,values1)
 
-  })
+
   return router;
 };
 
