329ba43981d559a082dac712cf5bd9d6089a5035
Slav Lalev <slalev57@me.com>
2019-11-20 20:54:25 +0000

    added te route for the owners  fetching data from the db in order to populate the page and added the html in order to display the values onto the page

--- routes/owner.js
+++ routes/owner.js
@@ -14,30 +14,39 @@ module.exports = (db) => {
     res.render("owners");
   });
 
-  router.get('/orders.id', (re, res) => {
+  router.get('/orders/:id', (req, res) => {
 
     //To display the person's order onto the page
-    db.query('SELECT * FROM orders RETURNING id', values)
-      .then(res => {
-        let order_id = res.rows[0].id;
+    let values = [req.params.id]
+    console.log(req.params.id)
+    db.query('SELECT * FROM orders WHERE id = $1', values)
+      .then(data => {
+        const order = data.rows[0]
+        // res.json(order)
 
+       db.query(`SELECT * FROM orders_items INNER JOIN items ON items.id = item_id WHERE order_id = $1`, values)
+       .then(data => {
+         order.items = data.rows
+         console.log("order", order);
+         res.render('owners', {order})
+       })
       })
-//to get the time to make from the text area
-let values = [`${req.body.waitingTime}`]
-db.query(`INSERT INTO orders (time_to_make) VALUES ($1)`,values)
+// //to get the time to make from the text area
+// let values1 = [`${req.body.waitingTime}`]
+// db.query(`INSERT INTO orders (time_to_make) VALUES ($1)`,values1)
 
   })
   return router;
 };
 
 
-// generates a random alphanumerical string of 6 characters
-function generateRandomString() {
-  var text = "";
-  var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < 6 ; i++)
-  text += charset.charAt(Math.floor(Math.random() * charset.length));
+// // generates a random alphanumerical string of 6 characters
+// function generateRandomString() {
+//   var text = "";
+//   var charset = "abcdefghijklmnopqrstuvwxyz0123456789";
+//   for (var i = 0; i < 6 ; i++)
+//   text += charset.charAt(Math.floor(Math.random() * charset.length));
 
-  return text;
-}
+//   return text;
+// }
 
